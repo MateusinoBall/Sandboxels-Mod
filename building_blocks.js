4d669af0972b8324c61788b0c504af0e42fb4038
Shirley Vital <shirleyvital245@gmail.com>
2026-08-09 15:18:00 -0300

    Add files via upload

---
+++ building_blocks.js
@@ -0,0 +1,39 @@
+Created by Mateusino Ball
+
+elements.cobblestone = {
+color: ["#7a7a7a", "#6b6b6b", "#5c5c5c"],
+behavior: behaviors.POWDER,
+category: "land",
+breakInto: "gravel",
+state: "solid",
+stateHigh: "polished_stone",
+tempHigh: 250,
+hardness: 0.5
+
+reactions: {
+"plasma": { elem1: "magma", elem2: "null" },
+"grass": { elem1: "dirt", elem2: "null" }
+}
+};
+
+if (!elements.cobblestone) {
+
+};
+}
+
+
+if (elements.magma) {
+
+
+elements.magma.reactions.stone = {
+elem1: ["stone", "basalt", "cobblestone"],
+chance: 0.05
+};
+
+
+if (elements.magma.reactions.water) {
+elements.magma.reactions.water.elem1 = ["stone", "basalt", "cobblestone"];
+}
+}
+
+});
