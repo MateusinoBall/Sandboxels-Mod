e0de361f0950df1c284ac03411fc551b40770104
Shirley Vital <shirleyvital245@gmail.com>
2026-08-10 20:31:06 -0300

    Modify building block states and reactions
    Updated reactions and properties for building blocks.

--- building_blocks.js
+++ building_blocks.js
@@ -9,11 +9,12 @@ behavior: behaviors.POWDER,
 category: "land",
 breakInto: "gravel",
   hardness: 0.5,
+  state: "solid",
   stateHigh: "polished_stone",
 tempHigh: 200,
 
 reactions: {
-"plasma": { elem1: "magma", elem2: "null" },
+"plasma": { elem1: "lava", elem2: "null" },
 "grass": { elem1: "dirt", elem2: "null" }
 };
 
@@ -24,8 +25,29 @@ elements.polished_stone = {
   breakInto: "polished_stone_fragments",
   hardness: 0.3,
   conduct: 0.1,
+  state: "solid"
+  stateHigh: "lava",
+  tempHigh: 1300,
+
+  reactions: {
+  "plasma": { elem1: "lava", elem2: "null" },
+  "acid": { elem1: "rock_wall", elem2: "neutral_acid" }
+};
+
+elements.polished_stone_fragments = {
+  color: ["#ced5d6", "#616161"],
+  behavior: behaviors.POWDER,
+  category: ""powders,
+  breakInto: ["stone", "carbon_dioxide"],
+  hardness: 0.3,
   state: "solid",
   stateHigh: "lava",
   tempHigh: 1300,
-  
-}
+
+  reactions: {
+  "god_ray": { elem1: "polished_stone", elem2: "null" },
+  "bless": { elem1: "polished_stone", elem2: "null" }
+  };
+
+elements.lava = {
+  color: ["#fcdf03", "#"]
