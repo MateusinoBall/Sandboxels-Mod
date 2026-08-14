a7fb8cb35dd2f8f0190a5e9b7a348d08b0842a72
Mateusino Ball <shirleyvital245@gmail.com>
2026-08-14 14:37:42 -0300

    Update reactions for dough and milk.

--- moreFoods.js
+++ moreFoods.js
@@ -1,8 +1,17 @@
 elements.dough.reactions = {
   ...elements.dough.reactions,
   "melted_chocolate": { elem1: "brigadeiro_dough", elem2: "null" },
-  "cheese_powder": { elem1: "cheese_bread", elem2: "null" },
-  "chicken": { elem1: "coxinha_dough", elem2: "null" },
+  "cheese_powder": { elem1: "cheese_bread", elem2: "cheese_croissant_dough" },
+  "chicken": { elem1: "coxinha_dough", elem2: "chicken_croissant_dough" },
   "yolk": { elem1: "pancakes_dough", elem2: "null" },
   "condensed_milk": { elem1: "donut_dough", elem2: "null" },
-  ""
+  "sauce": { elem1: "pizza_dough", elem2: "null" }
+};
+
+elements.milk.reactions = {
+  ...elements.milk.reactions,
+  "milk_cream": { elem1: "creamy_milk", elem2: "null" }
+};
+
+elements.brigadeiro_dough = {
+  color: ["#", "#"]
