2fd88b4e14c06c3d2c7f919c8c0cb32ab9878b2c
Mateusino Ball <shirleyvital245@gmail.com>
2026-08-14 13:52:57 -0300

    Add new reactions to dough elements

---
+++ moreFoods.js
@@ -0,0 +1,5 @@
+elements.dough.reactions = {
+  ...elements.dough.reactions,
+  "melted_chocolate": { elem1: "brigadeiro_dough", elem2: "null" },
+  "cheese_powder": { elem1: "cheese_bread", elem2: "null" },
+  ""
