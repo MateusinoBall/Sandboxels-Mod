813ad5c68ba2b2aad79d1b3db29f8e1029da4f4d
Mateusino Ball <shirleyvital245@gmail.com>
2026-08-16 21:11:49 -0300

    Fix null values in reactions for moreFoods.js
    Updated reactions to replace 'null' strings with null values.

--- moreFoods.js
+++ moreFoods.js
@@ -1,19 +1,19 @@
 elements.dough.reactions = {
   ...elements.dough.reactions,
-  "melted_chocolate": { elem1: "brigadeiro_dough", elem2: "null" },
+  "melted_chocolate": { elem1: "brigadeiro_dough", elem2: null },
   "cheese_powder": { elem1: "cheese_bread", elem2: "cheese_croissant_dough" },
   "chicken": { elem1: "coxinha_dough", elem2: "chicken_croissant_dough" },
-  "yolk": { elem1: "pancakes_dough", elem2: "null" },
-  "condensed_milk": { elem1: "donut_dough", elem2: "null" },
-  "sauce": { elem1: "pizza_dough", elem2: "null" }
+  "yolk": { elem1: "pancakes_dough", elem2: null },
+  "condensed_milk": { elem1: "donut_dough", elem2: null },
+  "sauce": { elem1: "pizza_dough", elem2: null }
 };
 
 elements.milk.reactions = {
   ...elements.milk.reactions,
-  "milk_cream": { elem1: "creamy_milk", elem2: "null" },
+  "milk_cream": { elem1: "creamy_milk", elem2: null },
   "sugar": { elem1: "condensed_milk", elem2: "sweet_particles" },
   "condensed_milk": { elem1: "sweetened_milk", elem2: "sugar" },
-  "sugar_water": { elem1: "milk_cream", elem2: "null" }
+  "sugar_water": { elem1: "milk_cream", elem2: null }
 };
 
 elements.brigadeiro_dough = {
