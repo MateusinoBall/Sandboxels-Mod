4c85a0764285ceb2c92cdded56ba49a0bcd1550a
Mateusino Ball <shirleyvital245@gmail.com>
2026-08-19 21:30:03 -0300

    Define reactions for wood and plant elements
    Added reactions for wood and plant elements with specific chances.

--- ecosystemstuff.js
+++ ecosystemstuff.js
@@ -18,8 +18,13 @@ elements.died_leafs = {
 
 if (elements.wood) {
   elements.wood.reactions = elements.wood.reactions || {};
-  elements.wood.reactions.air = { "chance": 0.001, element2: "died_leafs" }
-
+  elements.wood.reactions.air = { "chance": 0.001, element2: "died_leafs" };
+    }
 if (elements.plant) {
   elements.plant.reactions = elements.plant.reactions || {};
-  elements.plant.reactions.air = { "chance": 0.005, element2: "died_leafs" }
+  elements.plant.reactions.air = { "chance": 0.005, element2: "died_leafs" };
+}
+
+elements.decomposer_bacteria = {
+  color: "#6b8e23",
+  behavior: behaviors.FLUID,
