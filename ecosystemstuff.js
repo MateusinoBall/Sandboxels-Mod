55d3966f998a589fc2d4021a9bde61b414ad6529
Mateusino Ball <shirleyvital245@gmail.com>
2026-08-20 18:09:43 -0300

    Define decomposer_bacteria and poisonous_sap elements
    Added properties and reactions for decomposer_bacteria and poisonous_sap elements.

--- ecosystemstuff.js
+++ ecosystemstuff.js
@@ -28,3 +28,37 @@ if (elements.plant) {
 elements.decomposer_bacteria = {
   color: "#6b8e23",
   behavior: behaviors.FLUID,
+  category: "life",
+  breakInto: "dead_bug",
+  density: 1.1,
+  state: "solid",
+  stateHigh: "steam",
+  tempHigh: 250,
+  tempLow: -10,
+  stateLow: "frozen_meat",
+
+  reactions: {
+    "acid": { element1: "radiation", element2: null },
+    "died_leafs": { "chance": 0.01, element1: "mud", element2: "dirt" },
+    "poisonous_sap": { "chance": 0.5, element1: "rotten_meat", element2: "poison" },
+    "rotten_meat": { element1: "mud", element2: null },
+    "poison": { "chance": 0.001, element1: "flower_seed", element2: null },
+    "meat": { element1: "decomposer_bacteria", element2: "decomposer_bacteria" }
+  }
+};
+
+elements.poisonous_sap = {
+  color: "#a020f0",
+  behavior: behaviors.STURDYPOWDER,
+  category: "life",
+  state: "solid",
+  stateHigh: "poison",
+  tempHigh: 125,
+  tempLow: -20,
+  stateLow: "amber",
+
+  reactions: {
+    "vaccine": { element1: "sap", element2: null },
+    "plant": { element1: "sugar" }
+    }
+  };
