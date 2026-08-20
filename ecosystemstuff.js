8dcb3cc085120a978505ffbe2214358f5f47839e
Mateusino Ball <shirleyvital245@gmail.com>
2026-08-19 21:06:56 -0300

    Add died_leafs element with properties and reactions

--- ecosystemstuff.js
+++ ecosystemstuff.js
@@ -1,1 +1,20 @@
-ele
+elements.died_leafs = {
+  color: "#8b5a2b",
+  behavior: behaviors.POWDER,
+  category: "life",
+  density: 1.2,
+  state: "solid",
+  stateHigh: "fire",
+  tempHigh: 350,
+  desc: "died leafs waiting to be fertilized",
+
+  reactions: {
+    "decomposer_bacteria": { "chance": 0.01, element1: "mud", element2: "dirt" },
+    "life_worm": { "chance": 0.9, element1: "super_fertilizer", element2: "water" },
+    "acid": { element1: "carbon_dioxide", element2: null }
+  }
+};
+
+
+if (elements.wood) {
+  elements.wood.reactions = 
